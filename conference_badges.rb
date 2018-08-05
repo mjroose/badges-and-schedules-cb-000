971f805508eee1685ed592760040df114039f632
Michael Roose <mjroose@gmail.com>
2018-08-05 16:49:50 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,15 @@
-# Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(names)
+  #takes an array of names and calls #badge_maker for each name
+end
+
+def assign_rooms(names)
+  #takes an array of names and assigns each name a room number (index+1)
+end
+
+def printer(badges, room_assignments)
+  # should puts the badge and room assignment of each speakers
+end
